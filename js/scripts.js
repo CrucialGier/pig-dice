36b272a633a459435100c3f6dfbe514acec261fc
Georgina Van Dort and Stewart Cole <stewart@>
2016-06-15 09:43:06 -0700

    add dice roll functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
+var diceResult;
 var total = 0;
 
 var roll = function() {
- total = total += Math.floor((Math.random() * 6) + 1);
+  diceResult = Math.floor((Math.random() * 6) + 1);
+  total = total += diceResult;
 }
 
 
 $(document).ready(function() {
   $("#roll").click(function(){
     roll();
+    $("#dice-result").text(diceResult);
     $("#total").text(total);
   });
 
